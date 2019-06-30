39df6896c5ee77d2583d5b076c9dd07e97334289
gabrields1998 <gabrieldavidsacca@hotmail.com>
2019-06-30 20:52:20 -0300

    adição da logica do pet e do banco de dados

---
+++ lib/main-bloc.dart
@@ -0,0 +1,138 @@
+import 'dart:math';
+
+class Pet {
+  var _id;
+  var _name;
+  var _lastTime;
+  var _state;
+  var _happy;
+  var _hunger;
+  var _health;
+  var _sleep;
+  var _dirty;
+
+  
+  Pet(this._name, this._lastTime, this._state, this._happy, this._hunger, this._health, this._sleep, this._dirty);
+
+  Pet.map(dynamic obj) {
+    // quando passamos um nome ele já recebe como objeto
+    this._id = obj['Id'];
+    this._name = obj['Name'];
+    this._happy = obj['Happy'];
+    this._hunger = obj['Hunger'];
+    this._health = obj['Health'];
+    this._sleep = obj['Sleep'];
+    this._dirty = obj['Dirty'];
+    this._state = obj['State'];
+    this._lastTime = obj['LastTime'];
+  } 
+
+  // o map irá acessar uma key, como string, e depois uma chave que eu deseje
+  Map<String, dynamic> toMap() {
+    //instanciando o mapa
+    var mapa = new Map<String, dynamic>();
+    // agora iremos fazer o inverso do anterio, iremos colocar o conteúdo 
+    // dos atributos no mapa, ficará como se fosse um json
+    mapa["Name"] = _name;
+    mapa["Happy"] = _happy;
+    mapa["Hunger"] = _hunger;
+    mapa["Health"] = _health;
+    mapa["Sleep"] = _sleep;
+    mapa["Dirty"] = _dirty;
+    mapa["State"] = _state;
+    mapa["LastTime"] = _lastTime;
+
+    // verificar se o id tem alguma coisa, para não adicionar nada em branco
+    if (_id != null){
+      mapa["id"] = _id;
+    }
+    return mapa;
+ }
+
+ Pet.fromMap(Map<String, dynamic> mapa) {
+    // pegarei o mapa que estamos recebendo da funcao acima e colocando em _nome
+    this._name = mapa['Name'];
+    this._id = mapa['id'];
+    this._happy = mapa['Happy'];
+    this._hunger = mapa['Hunger'];
+    this._health = mapa['Health'];
+    this._sleep = mapa['Sleep'];
+    this._dirty = mapa['Dirty'];
+    this._state = mapa['State'];
+    this._lastTime = mapa['LastTime'];
+  }
+ 
+  int getHappy() => _happy;
+  int getHunger() => _hunger;
+  int getHealth() => _health;
+  int getSleep() => _sleep;
+  int getDirty() => _dirty;
+  String getState() => _state;
+  int getId() => _id;
+
+  void setHappy(happy) => _happy += happy;
+  void setHunger(hunger) => _hunger += hunger;
+  void setHealth(health) => _health += health;
+  void setSleep(sleep) => _sleep += sleep;
+  void setDirty(dirty) => _dirty += dirty;
+  void setLastTime(lastTime) => _lastTime = lastTime;
+
+
+  update() {
+    var dateNow = new DateTime.now();
+    var deltaTime =  dateNow.difference(DateTime.parse(_lastTime)).inMinutes; // delta desde útimo update()
+    print("$deltaTime");
+    var heightHunger = 1, heightHealth = 1, heightHappy = 1, heightDirty = 1, heightSleep = 1;
+
+    // máquina de estados do vpet
+    if (_state == 'sick') {
+      heightHunger = 3;
+      heightHealth = 3;
+      heightHappy = 3;
+      heightDirty = 3;
+      heightSleep = 3;
+    }else if (_state == 'tired' || _state == 'dirty') {
+      heightHappy = 2;
+    }else if(_state == 'sad') {
+      heightHealth = 2;
+    }else if(_state == 'sleeping') {
+      heightHunger = 2;
+      heightSleep = -3;
+    }else if(_state == 'hunger') {
+      heightHealth = 2;
+      heightSleep = 2;
+      heightHappy = 3;
+    }
+
+    // taxas estão relacionadas ao estado atual do Pet
+    var hungerRate = 5, healthRate = 4, happyRate = 3, dirtyRate = 4, sleepRate = 3;
+    // atualiza itens de status (versão "muito simples")
+    Random rnd;
+    rnd = new Random();
+
+    _hunger -= (hungerRate * rnd.nextDouble() * heightHunger * deltaTime ).toInt();
+    _health -= (healthRate * rnd.nextDouble() * heightHealth * deltaTime).toInt();
+    _happy -= (happyRate * rnd.nextDouble() * heightHappy * deltaTime).toInt();
+    _dirty -= (dirtyRate * rnd.nextDouble() * heightDirty * deltaTime).toInt();
+    _sleep -= (sleepRate * rnd.nextDouble() * heightSleep * deltaTime).toInt();
+
+    if(_sleep > 100) _sleep = 100;
+    if(_hunger > 100) _hunger = 100;
+    if(_health > 100) _health = 100;
+    if(_happy > 100) _happy = 100;
+    if(_dirty > 100) _happy = 100;
+
+    // atualiza estados
+    if (_happy < 25) _state = 'sad';
+    else if (_health < 25) _state = 'sick';
+    else if(_dirty < 25 ) _state = 'dirty';
+    else if(_sleep < 25) _state = 'tired';
+    else if(_hunger < 25) _state = 'hunger';
+    else if(_happy >= 25 && _health >= 25 && _hunger >= 25 && _sleep >= 25) _state = 'normal';
+    if (_happy <= 0 || _health <= 0 || _hunger <= 0 || _sleep <= 0) _state = 'dead';
+
+    setLastTime(new DateTime.now());
+    print("$_state");
+  }
+  
+}
---
+++ lib/ui/bd.dart
@@ -0,0 +1,147 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import "../main-bloc.dart";
+
+class BD {
+ // criaremos uma variável static, pois nunca irá mudar
+ static final BD _instance = new BD.internal();
+
+ // criaremos uma classe factory porque não será recriada sempre que chamarmos a classe BD (POO)
+ factory BD() => _instance;
+
+ // internal é um construtor então toda vez que precisamos é só instanciá-lo
+ BD.internal();
+
+ // criando o esqueleto da nossa tabela do banco de dados
+ final String tabelaPet = "TabelaPet";
+ final String colunaId = "Id";
+ final String colunaName = "Name";
+ final String colunaHappy = "Happy";
+ final String colunaHunger = "Hunger";
+ final String colunaHealth = "Health";
+ final String colunaDirty = "Dirty";
+ final String colunaSleep = "Sleep";
+ final String colunaState = "State"; 
+ final String colunaLastTime = "LastTime";
+
+ //Database é a classe do SQFlite que iremos usar, por isso iremos criá-la
+ static Database _db;
+
+  // sempre que formos acessar alguma coisa utilizar o future, pois ele é uma transação alheia
+  Future<Database> get db async {
+    // se o _db existe na memória
+    if(_db != null){
+      //caso exista, retorna este _bd existente
+      return _db;
+    }
+    // chamamos agora o initBd que irá iniciar o nosso banco de dados
+    _db = await initBd();
+    return _db;
+  }
+
+  // iniciando nosso banco de dados em async pois ele é uma transição
+  initBd() async {
+    // Directory faz parte do plugin dart:io e o getApplicationDocumentsDirectory() faz parte do path_provider
+    // aqui nós estamos acessando o diretório nativo do android
+    Directory documentoDiretorio = await getApplicationDocumentsDirectory();
+
+    // o join() junta duas coisas, no caso iremos juntar o diretorio juntamente com o nosso banco de dados
+    String caminho = join(
+      documentoDiretorio.path, "bd_principal.db"
+    );
+
+    // após ter acesso ao local do nosso BD, iremos abri-lo
+    var nossoBD = await openDatabase(caminho, version: 1, onCreate: _onCreate);
+    return nossoBD;
+ 
+  }
+
+  //criando o método _onCreate que irá criar o nosso BD
+  void _onCreate(Database db, int version) async {
+    // aqui iremos colocar o SQL que é outra linguagem, por isso, colocaremos
+    // dentro de aspas, pois é string
+    await db.execute("CREATE TABLE $tabelaPet($colunaId INTEGER PRIMARY KEY,"
+      "$colunaName TEXT,"
+      "$colunaHappy INTEGER,"
+      "$colunaHunger INTEGER,"
+      "$colunaHealth INTEGER,"
+      "$colunaDirty INTEGER,"
+      "$colunaSleep INTEGER,"
+      "$colunaState INTEGER,"
+      "$colunaLastTime TEXT)");
+  }
+
+  Future<int> inserirPet(Pet pet) async {
+  // o db aqui é do get escrito lá no início
+  // ao inserirmos um usuario, temos que criar um já esta instância de db
+  // esse db irá retornar um database
+  var bdCliente = await db;
+
+  // para inserir temos que passar uma tabela e valores que pode ser um mapa
+  // ese mapa é aquele que criamos em usuarios.dart
+  int res = await bdCliente.insert("$tabelaPet", pet.toMap(), conflictAlgorithm: ConflictAlgorithm.replace);
+
+  // aqui irá retornar um número
+  return res;
+  }
+
+ // para podermos pegar os usuarios criados
+  Future<List> pegarPets() async {
+    // praticamente iremos fazer a mesma coisa da função de inserir
+    var bdCliente = await db;
+
+    // rawQuery é como se fosse um pedido, então iremos solicitar todos os dados
+    var res = await bdCliente.rawQuery("SELECT * FROM $tabelaPet");
+    // agora iremos retornar uma list
+    return res.toList();
+ }
+
+ Future<int> pegarContagem() async {
+    var bdCliente = await db;
+    // para pegarmos a contagem temos que ir na SQFlite e utilizar o firstIntValue
+    return Sqflite.firstIntValue(await bdCliente.rawQuery("SELECT COUNT(*) FROM $tabelaPet"));
+  }
+
+  // iremos mostrar somente um usuário
+  Future<Pet> pegarUnicoPet(int id) async {
+    var bdCliente = await db;
+    var res = await bdCliente.rawQuery("SELECT * FROM $tabelaPet"
+              " WHERE $colunaId = $id"); 
+
+    // verificando se a lista retorna nada
+    if (res.length == 0) return null;
+
+    // iremos retornar um mapa dos dados, pega só o primeiro 
+    return new Pet.fromMap(res.first);
+ }
+
+  Future<int> apagarPet(int id) async {
+    var bdCliente = await db;
+
+    // where é o local de onde iremos deletar os dados
+    return await bdCliente.delete(tabelaPet,
+      where: "$colunaId = ?", whereArgs: [id]);
+  }
+
+  Future<int> editarPet(Pet pet) async {
+    var bdCliente = await db;
+
+    // usaremos o toMap
+    return await bdCliente.update(tabelaPet,
+      pet.toMap(), where: "$colunaId = ?", whereArgs: [pet.getId()]
+    );
+  } 
+
+   deleteOptionTable() async {
+    final bdCliente = await db;
+    bdCliente.rawDelete("DROP TABLE IF EXISTS *");
+  }
+
+  Future fechar() async {
+    var bdCliente = await db;
+
+    return bdCliente.close();
+  } 
+}
